4d538a530585b69b37d1be51608ee74a7566893c
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2022-03-02 23:04:07 -0300

    test: all ones game

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -1,13 +1,23 @@
 import { Game } from './game'
 
 describe('Boliche', () => {
-  const game = new Game()
-
   test('quando o jogador não acerta nenhum pino', () => {
+    const game = new Game()
+
     for(let i=0; i<20; i++) {
       game.jogar(0)
     }
 
     expect(game.score()).toBe(0)
+  })
+
+  test('quando o jogador acerta um pino em todas as jogadas', () => {
+    const game = new Game()
+
+    for(let i=0; i<20; i++) {
+      game.jogar(1)
+    }
+
+    expect(game.score()).toBe(20)
   })
 })
