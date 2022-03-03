5848c4f6c702e77de95f6853ed4460a53a8162ca
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2022-03-02 23:45:06 -0300

    refactor: improve game design with score helper methods

--- src/game.ts
+++ src/game.ts
@@ -12,14 +12,14 @@ export class Game {
 
     for(let frame=0; frame<10; frame++) {
       if(this.ehStrike(frameIndex)) {
-        _score = 10 + this.jogadas[frameIndex + 1] + this.jogadas[frameIndex + 2]
+        _score = this.bonusStrike(frameIndex)
         frameIndex += 1
       }
       if(this.ehSpare(frameIndex)) {
-        _score += 10 + this.jogadas[frameIndex + 2]
+        _score += this.bonusSpare(frameIndex)
         frameIndex += 2
       } else {
-        _score += this.jogadas[frameIndex] + this.jogadas[frameIndex + 1]
+        _score += this.jogadaNormal(frameIndex)
         frameIndex += 2
       }
     }
@@ -33,5 +33,17 @@ export class Game {
 
   private ehSpare(frameIndex: number) : boolean {
     return this.jogadas[frameIndex] + this.jogadas[frameIndex + 1] === 10
+  }
+
+  private bonusStrike(frameIndex: number) : number {
+    return 10 + this.jogadas[frameIndex + 1] + this.jogadas[frameIndex + 2]
+  }
+
+  private bonusSpare(frameIndex: number) : number {
+    return 10 + this.jogadas[frameIndex + 2]
+  }
+
+  private jogadaNormal(frameIndex: number) : number {
+    return this.jogadas[frameIndex] + this.jogadas[frameIndex + 1]
   }
 }
